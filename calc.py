23c739c04265b5ab6fcaf4bb337ebd76e52b618b
Alan <aalijson@gmail.com>
2022-06-13 17:44:18 +0600

    calculator

--- calc.py
+++ calc.py
@@ -3,10 +3,17 @@ b = 'Введите второе число: '
 oper = 'Выберите операцию из следующих +-*/%**// : '
 res = 'Ответ:'
 miss = 'Данной операции нет в системе'
+miss0 = 'На ноль делить нельзя'
 a1 = float(input(a))
 b1 = float(input(b))
 operation = input(oper)
-if operation == '+':
+if b1 == 0 and operation == '/':
+  print (miss0)
+elif b1 == 0 and operation == '//':
+  print (miss0)
+elif b1 == 0 and operation == '%':
+  print (miss0)
+elif operation == '+':
   print (res, a1 + b1)
 elif operation == '-':
   print (res, a1 - b1)
@@ -22,3 +29,4 @@ elif operation == '//':
   print (res, a1 // b1)
 else:
   print (miss)
+  
